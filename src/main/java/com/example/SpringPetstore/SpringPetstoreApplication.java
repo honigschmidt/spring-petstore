6e921ed9abe146da302e6b03aa95d527d39c1c85
admin <admin@nomail.com>
2023-04-24 08:54:55 +0200

    Update

--- src/main/java/com/example/SpringPetstore/SpringPetstoreApplication.java
+++ src/main/java/com/example/SpringPetstore/SpringPetstoreApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class SpringPetstoreApplication {
 
--- src/main/java/com/example/SpringPetstore/controller/entity/UserController.java
+++ src/main/java/com/example/SpringPetstore/controller/entity/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
